3906343b18aaa0743ef050d5505efe4b3092c89e
KyokoSamura <samura.kyoko@gmail.com>
2021-01-16 18:22:08 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <Header></Header>      
+     <p>{{item}}</p>
+     <div id="app">
+       <p>Address:</p>
+     </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import axios from "axios";
+export default {
+  data() {
+    return {
+      
+      text: ""
+    };
+  },
+  methods:{
+      async item() {
+      const item = await axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/郵便番号apiKey=V0DXRtKvacz59kMqfc2RTVZIeUuNpLB0PhtqsaX`);
+      console.log(item);
+     }
+  },
+  components: {
+    Header
+  }
+}
+</script>
